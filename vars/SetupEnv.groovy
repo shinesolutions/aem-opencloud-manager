00f07f5bb1dd5492754b26716476560967bbb6a4
Cliffano Subagio <cliffano@gmail.com>
2019-02-15 08:50:55 +1100

    Fix double script declaration.

--- vars/SetupEnv.groovy
+++ vars/SetupEnv.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(script) {
-  // Install awscli which is required by the other pipelines.
-  script.sh """script.sh """echo 'Setting up environment...'
+  script.sh """
+    echo 'Setting up environment...'
   """
 }
