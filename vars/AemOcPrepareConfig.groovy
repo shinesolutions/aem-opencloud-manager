783c0b91f485b2526cd367e6968ea30e1dbaac5f
Cliffano Subagio <cliffano@gmail.com>
2019-02-15 14:57:13 +1100

    Rename oc prefix with aoc for consistency.

--- vars/AemOcPrepareConfig.groovy
+++ vars/AemOcPrepareConfig.groovy
@@ -3,13 +3,13 @@ import common.*
 
 def call(script, String downloadUrl, String tmpDir = '/tmp') {
   filename = "aem_opencloud_config.tar.gz"
-  oc_config_dir = "${tmpDir}/opencloud_config"
+  aoc_config_dir = "${tmpDir}/opencloud_config"
   script.sh """
   mkdir -p ${tmpDir}/
   """
   new common().httpDownload(script, downloadUrl, tmpDir, filename)
   script.sh """
-  mkdir -p ${oc_config_dir}/
-  tar --strip-components=1 -xzf ${tmpDir}/${filename} -C ${oc_config_dir}/
+  mkdir -p ${aoc_config_dir}/
+  tar --strip-components=1 -xzf ${tmpDir}/${filename} -C ${aoc_config_dir}/
   """
 }
