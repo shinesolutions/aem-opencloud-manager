54a11504ff38f818c91cf6e4ab601092f34ce536
Cliffano Subagio <cliffano@gmail.com>
2019-02-17 16:16:53 +1100

    Add comment, rename vars for groovy consistency.

--- vars/AocPrepareConfig.groovy
+++ vars/AocPrepareConfig.groovy
@@ -1,15 +1,20 @@
 #!/usr/bin/env groovy
 import common.*
 
+/**
+ * This script downloads AEM OpenCloud configuration artifact and unpacks it
+ * on temp directory, to be referenced as config_path by other AEM OpenCloud
+ * libraries (e.g. Packer AEM, AEM AWS Stack Builder).
+ */
 def call(script, String downloadUrl, String tmpDir = '/tmp') {
-  filename = "aem-opencloud-config.tar.gz"
-  aoc_config_dir = "${tmpDir}/aem-opencloud-config"
+  fileName = "aem-opencloud-config.tar.gz"
+  aocConfigDir = "${tmpDir}/aem-opencloud-config/"
   script.sh """
   mkdir -p ${tmpDir}/
   """
-  new common().httpDownload(script, downloadUrl, tmpDir, filename)
+  new common().httpDownload(script, downloadUrl, tmpDir, fileName)
   script.sh """
-  mkdir -p ${aoc_config_dir}/
-  tar --strip-components=1 -xzf ${tmpDir}/${filename} -C ${aoc_config_dir}/
+  mkdir -p ${aocConfigDir}/
+  tar --strip-components=1 -xzf ${tmpDir}/${fileName} -C ${aocConfigDir}/
   """
 }
