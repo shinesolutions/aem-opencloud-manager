81e0a41a607beb2f309305342c25e2513294ce3b
Cliffano Subagio <cliffano@gmail.com>
2019-02-17 16:19:58 +1100

    Rename Git prefix to Github since the download source is specific to GitHub and not generic Git.

--- vars/AwsMirrorGithubRepo.groovy
+++ vars/AwsMirrorGithubRepo.groovy
@@ -3,6 +3,6 @@ import common.*
 import cloud.aws
 
 def call(script, String user, String repo, String version, String s3Endpoint, String tmpDir = '/tmp') {
-  GitDownloadRelease(script, user, repo, version, tmpDir)
+  GithubDownloadRelease(script, user, repo, version, tmpDir)
   AwsS3Upload(script, repo, version, s3Endpoint, tmpDir)
 }
--- vars/GithubDownloadRelease.groovy
+++ vars/GithubDownloadRelease.groovy
@@ -1,11 +1,14 @@
 #!/usr/bin/env groovy
 import common.*
 
+/**
+ * This script downloads an artifact from GitHub to temp directory.
+ */
 def call(script, String user, String repo, String version, String tmpDir = '/tmp') {
   archive_url = "https://github.com/${user}/${repo}/releases/download/${version}/${repo}-${version}.tar.gz"
-  filename = "${repo}-${version}.tar.gz"
+  fileName = "${repo}-${version}.tar.gz"
   script.sh """
   mkdir -p ${tmpDir}
   """
-  new common().httpDownload(script, archive_url, tmpDir, filename)
+  new common().httpDownload(script, archive_url, tmpDir, fileName)
 }
