40a7a5aa864aeb7fa4c0868bf5fedaed19c06780
Cliffano Subagio <cliffano@gmail.com>
2019-02-18 01:24:17 +1100

    Fix destDir param name.

--- vars/AocPrepareLibrary.groovy
+++ vars/AocPrepareLibrary.groovy
@@ -6,7 +6,7 @@ import cloud.aws
  * on temp directory.
  */
 def call(script, String artifact, String version, String s3Bucket, String s3Path, String tmpDir) {
-  new aws().s3_download(script, s3Bucket, s3Path, "${artifact}-${version}.tar.gz", destDir)
+  new aws().s3_download(script, s3Bucket, s3Path, "${artifact}-${version}.tar.gz", tmpDir)
   script.sh """
   mkdir -p ${tmpDir}/${artifact}/
   tar -xzf ${tmpDir}/${artifact}-${version}.tar.gz -C ${tmpDir}/${artifact}/
