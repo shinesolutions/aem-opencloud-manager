daaed9b8bef84dab012ce0d2c18c9bbab2be13aa
Cliffano Subagio <cliffano@gmail.com>
2019-03-04 23:24:30 +1100

    Add empty URL handling, add unsupported URI scheme handling.

--- vars/AocPrepareConfig.groovy
+++ vars/AocPrepareConfig.groovy
@@ -8,23 +8,37 @@ import cloud.aws
  * AEM OpenCloud libraries (e.g. Packer AEM, AEM AWS Stack Builder).
  */
 def call(script, String aocConfigDownloadUrl, String tmpDir = '/tmp') {
+
+  if(aocConfigDownloadUrl?.trim ()) {
+    println "Skipping AEM OpenCloud configuration download due to unspecified URL"
+    return
+  }
+
   fileName = "aem-opencloud-config.tar.gz"
   aocConfigDir = "${tmpDir}/aem-opencloud-config/"
   script.sh """
   mkdir -p ${tmpDir}
   """
+
   def parsedUri = new URI(aocConfigDownloadUrl)
   switch (parsedUri.scheme) {
     case "http":
       new common().httpDownload(script, aocConfigDownloadUrl, tmpDir, fileName)
+      break
     case "https":
       new common().httpDownload(script, aocConfigDownloadUrl, tmpDir, fileName)
+      break
     case "s3":
       def bucket = parsedUri.host
       def path = parsedUri.path.split('/').init().drop(1).join('/')
       def object = parsedUri.path.split('/').last()
       new aws().s3_download(script, bucket, path, object, tmpDir, fileName)
+      break
+    default:
+      println "Unsupported AEM OpenCloud configuration download URL: " + aocConfigDownloadUrl
+      break
   }
+
   script.sh """
   mkdir -p ${aocConfigDir}/
   tar --strip-components=1 -xzf ${tmpDir}/${fileName} -C ${aocConfigDir}/
