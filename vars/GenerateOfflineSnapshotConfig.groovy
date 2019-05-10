2de37443fef9cb42910dc50e31d04498249845e6
Eng Nuth <eng.nuth@shinesolutions.com>
2019-05-10 11:41:09 +1000

    Default value for sourceStackPrefix snould be null not emptry string

--- vars/GenerateOfflineSnapshotConfig.groovy
+++ vars/GenerateOfflineSnapshotConfig.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 import cloud.aws
 
-def call(script, String sourceAuthorSnapshotId = null , String sourcePublishSnapshotId = null, String sourceStackPrefix = '', String configPath) {
+def call(script, String sourceAuthorSnapshotId = null , String sourcePublishSnapshotId = null, String sourceStackPrefix = null, String configPath) {
   if ( sourceAuthorSnapshotId || sourcePublishSnapshotId ) {
     GenerateOfflineSnapshotYaml(this, sourceAuthorSnapshotId, sourcePublishSnapshotId, configPath)
   } else if ( sourceStackPrefix ) {
